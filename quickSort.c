5c3203fc51d596a3993394a35911d6b32eb13e13
mouminamajeed <81755364+mouminamajeed@users.noreply.github.com>
2022-04-21 17:21:26 +0530

    Update quickSort.c

--- quickSort.c
+++ quickSort.c
@@ -1,58 +1,48 @@
-#include <stdio.h>
- 
-void quicksort (int [], int, int);
- 
-int main()
+#include<stdio.h>
+void quicksort (int a[40],int first, int last)
 {
-    int a[50];
-    int n, i;
- 
-    printf("Enter the number of elements: ");
-    scanf("%d", &n); 
-    printf("Enter the elements to be sorted:\n");
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    } 
-    quicksort(a, 0, n - 1);
-    printf("After applying quick sort\n");
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
- 
-    return 0;
+int i, j, pivot, temp;//pivot fr sorting
+
+if (first<last)
+{
+
+pivot=first;//0th elemnt
+i=first;
+j=last;
+while (i<j)
+{
+while (a [i]<=a [pivot] &&i<last)
+i++;
+while (a[j]>a[pivot])
+j--;
+ //at prtculr point it wll nt decrse as i inc and j dec
+if (i<j)
+{
+temp =a [i];
+a [i]=a [j];
+a[j]=temp;
 }
-void quicksort(int a[], int low, int high)
+}
+ //when i crosses j i.e i>j cond bcm false ,elmnt in pvt swapd with indx el j
+temp=a[pivot];
+a[pivot]=a[j];
+a[j]=temp;
+quicksort(a,0,j-1);//call fr lft elmnts,in lft indx strt with0 to pvt index-1
+quicksort(a,j+1,last);//fr rght el, call recursvl yuntil crct pos of indx
+}
+}
+int main ()
 {
-    int pivot, i, j, temp;
-    if (low < high)
-    {
-        pivot = low;
-        i = low;
-        j = high;
-        while (i < j) 
-        {
-            while (a[i] <= a[pivot] && i <= high)
-            {
-                i++;
-            }
-            while (a[j] > a[pivot] && j >= low)
-            {
-                j--;
-            }
-            if (i < j)
-            {
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-            }
-        }
-        temp = a[j];
-        a[j] = a[pivot];
-        a[pivot] = temp;
-        quicksort(a, low, j - 1);
-        quicksort(a, j + 1, high);
-    }
+
+int a[40],n,i;
+printf("enter sizeof array:-");
+scanf(" %d", &n);
+printf ("enter elements: ");
+for (i=0; i<n; i++)
+scanf ("%d", &a[i]);
+quicksort (a, 0, n-1) ;
+printf ("sorted elements are: ") ;
+for (i=0; i<n; i++)
+printf (" %d", a [i]);
+return 0;
 }
